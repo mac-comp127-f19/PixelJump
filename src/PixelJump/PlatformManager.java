5cb1397af1547dbb82ec1eec6eaf2c7d83fce952
WeishiD <dingweishi0001@hotmail.com>
2019-11-21 08:57:59 -0600

    working on PlatformManager

--- src/PixelJump/PlatformManager.java
+++ src/PixelJump/PlatformManager.java
@@ -1,5 +1,36 @@
 package PixelJump;
+import comp127graphics.CanvasWindow;
+import comp127graphics.GraphicsGroup;
+import comp127graphics.GraphicsObject;
+
+import java.util.Random;
 
 public class PlatformManager {
+private CanvasWindow canvas;
+private GraphicsGroup platformCollection;
+private double platformWidth;
+private double platformHeight;
+private static final int Jumprange=100;
+private Random rand;
+ public PlatformManager(CanvasWindow canvas){
+     this.canvas=canvas;
+     this.platformWidth=(canvas.getWidth()*0.1);
+     this.platformHeight=(canvas.getHeight()*0.1);
+     rand=new Random();
+ }
+
+ public void generateBricks(){
+
+ }
+
+
+
+ public void removeSinglePlatform(){
+
+ }
+ public void removeAllPlatform(){
+
+ }
+
 
 }
